3103ae5c9f3bf30443e69dbbf98c255cd1802eaf
Cian Monnin <cianmonnin@gmail.com>
2024-07-09 10:58:26 -0400

    fix: tweak schema_script.sh

--- schema_script.sh
+++ schema_script.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 schema_urls=(
-"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=20"
-"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=12"
-"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=5"
-"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=6"
-"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=7"
-"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=8"
-"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=11"
+"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=1"  # Data field properties
+"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=5"  # Values for simple integer encodings
+"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=6"  # Values for simple string encodings
+"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=7"  # Values for simple real (floating-point encodings)
+"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=8"  # Values for simple data encodings
+"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=11" # Values for hierarchical string encodings
+"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=12" # Values for hierarchical string encodings
+"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=13" # Catgegory browse tree structure
+"https://biobank.ndph.ox.ac.uk/ukb/scdown.cgi?fmt=txt&id=20" # Values for simple time encodings
 )
 
 for url in "${schema_urls[@]}"; do
   id=$(echo "$url" | grep -oP '(?<=id=)\d+')
-  filename="schema${id}.txt"
+  filename="schema${id}.tsv"
   
   echo $url
   curl -o "$filename" "$url"
 
   echo "Downloaded schema ${id} to ${filename}"
 done
+
+echo "Downloads complete"
